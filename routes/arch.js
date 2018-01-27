fd96da2a2ae19216813b469a46e3282a1a49a5e8
babylee2002 <babylee2002@gmail.com>
2018-01-27 16:32:09 -0500

    post arch works

--- routes/arch.js
+++ routes/arch.js
@@ -7,9 +7,9 @@ const Arch = require('../db/models/Arch')
 router.get('/', async (req, res) => {
     try {
         const city = await City.findById(req.params.cityId)
-        console.log('FromArchRoute:'+city)
+        // console.log('GetFromArchRoute:'+city)
         const showArch = city.arch
-        console.log('Find:'+city.arch)
+        // console.log('Get:'+city.arch)
         res.json(showArch)
     } catch (error) {
         console.log(error)
@@ -19,27 +19,29 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const newArch = new aArch(req.body.arch)
-        console.log('newArchPostRoute:'+req.body.arch)
+        const newArch = new Arch(req.body)
+        console.log('newArchPostRoute:'+JSON.stringify(req.body))
         const city = await City.findById(req.params.cityId)
+        console.log('FromArchPostRoute:'+JSON.stringify(city))
         city.arch.push(newArch)
-        const postArch = await City.save()
-        console.log('FromArchPostRoute:'+postArch)
+        
+        const postArch = await city.save()
+        
         res.json(postArch)
     } catch (err) {
         res.send(err)
     }
 })
 
-router.delete('/:archId', async (req, res) => {
-    try {
-        const city = await City.findById(req.params.cityId)
-        city.arch.id(req.params.archId).remove()
-        const saved = await city.save()
-        res.json(saved)
-    } catch (err) {
-        res.send(err)
-    }
-})
+// router.delete('/:archId', async (req, res) => {
+//     try {
+//         const city = await City.findById(req.params.cityId)
+//         city.arch.id(req.params.archId).remove()
+//         const saved = await city.save()
+//         res.json(saved)
+//     } catch (err) {
+//         res.send(err)
+//     }
+// })
 
 module.exports = router;
